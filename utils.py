462d7188676767a80b84aa2fe024b49ce7a1d917
radhacr <radha.cr@gmail.com>
2022-11-11 01:38:55 -0500

    Error corrections in concept extraction

--- utils.py
+++ utils.py
@@ -1,18 +1,25 @@
 def extract_concepts(data):
     from pymetamap import MetaMap
-    MetaMapPath = '/Users/radhachitta/pehr/PatientAssessment/public_mm/bin/metamap16'
+    MetaMapPath = '/Users/radhachitta/pehr/PatientAssessment/public_mm/bin/metamap'
     mm = MetaMap.get_instance(MetaMapPath)
     data = data.encode("ascii","ignore").decode()
-    concepts,error = mm.extract_concepts([data],[1,2],
+    concepts,error = mm.extract_concepts([data],ids=None,
         restrict_to_sts = ['sosy','fndg','acty','bhvr','biof','bpoc','clna',
         'clnd','cnce','diap','dora','drdd','dsyn','food','hlca','inbe','inpo','lbpr','lbtr','mobd',
         'virs','vita'])
 
     return concepts
 
+def process_concept(concept):
+    if concept.__class__.__name__ == 'ConceptMMI':
+            preferred_name = concept.preferred_name
+            return(preferred_name.split('(')[0].rstrip() + " (" + preferred_name + "->" +  concept.cui + "->" + concept.semtypes + ")")
+    if concept.__class__.__name__ == 'ConceptAA':
+            return(concept.long_form + " (" + concept.short_form + ")")
+
 def pp_concepts(data):
     concepts = extract_concepts(data)
     results = []
     for concept in concepts:
-        results.append(concept.preferred_name.split('(')[0].rstrip() + " (" + concept.preferred_name + "->" +  concept.cui + "->" + concept.semtypes + ")\n")
+        results.append(process_concept(concept) + "\n")
     return results
