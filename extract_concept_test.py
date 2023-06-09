201dbe73633d109dd900222e605625acd011ea39
radhacr <radha.cr@gmail.com>
2023-06-09 12:14:36 -0400

    Adding additional health information

--- extract_concept_test.py
+++ extract_concept_test.py
@@ -1,13 +1,18 @@
 import sys
 from utils import *
 import openai
-openai.api_key = "open-api-key"
+import argparse
+
+#openai.api_key = "open-api-key"
+
 # Read the file containing the text
-if len(sys.argv) <  2:
-    print ("Need an input filename containing the notes.")
-    sys.exit()
-input_file = sys.argv[1]
-#input_file="sample3.txt"
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', "--input_file", help="File containing the symptoms", required=True)
+parser.add_argument('-a', "--additional_file", help="File containing any addiotional conditions obtained from medical history or vitals information", default="")
+args = vars(parser.parse_args())
+input_file = args["input_file"]
+additional_file = args["additional_file"]
+
 
 with open (input_file, 'r') as file:
     data  = file.read().rstrip()
@@ -18,12 +23,22 @@ print(data)
 # for result in metamap_results:
 #     print (result)
 
-prompt1 = "Read the following text containing the symptoms I am experiencing delimited by backticks:\n```"
-prompt2 = "\n```\nIn the form of an unnumbered list, tell me what diagnoses the symptoms could indicate " \
+request = "Read the following text containing the symptoms I am experiencing delimited by backticks:\n```\n" + data + "\n```\n" 
+
+if additional_file != "":
+	with open(additional_file, 'r') as f:
+		health_conditions = f.readlines()
+	request = request + "\nThe following text delimited by hashes contains my health status:\n###\n" 
+	for i in range(len(health_conditions)):
+		request = request + str(i+1) + ". " + health_conditions[i].rstrip() + "\n"
+	request = request + "###\n"
+
+request = request +  "\nIn the form of an unnumbered list, tell me what diagnoses the symptoms could indicate " \
          "starting from the most likely to the least likely?  Also list  the percentage " \
          "of chance for each diagnosis. Do not provide explanation of the diagnoses."
 
-request = prompt1 + data + prompt2
+
+print (request)
 
 model = "gpt-3.5-turbo"
 
@@ -49,7 +64,8 @@ if response['choices'][0]['finish_reason'] == "stop":
 else:
     print ("unable to determine the diagnosis")
 
-print ("Conditions from OPENAI:")
+#print ("Conditions from OPENAI:")
+print ("Diagnoses")
 
 for diag in prelim_diagnosis:
     print (diag + ":" + prelim_diagnosis_perc[diag])
